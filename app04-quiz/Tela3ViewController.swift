7bed3854db50fc725414b7604a3b533a1edbb085
leonelmendes <leonelftacisco@hotmail.com>
2024-12-25 21:10:54 +0000

    create all pages, only just the logic

---
+++ app04-quiz/Tela3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Tela3ViewController.swift
+//  app04-quiz
+//
+//  Created by Aluno ISTEC on 25/12/2024.
+//
+
+import UIKit
+
+class Tela3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
